44a5549f32e1260eefcecf13bec649bfa537f026
Justin Stroud <157166970+justinstroudbah@users.noreply.github.com>
2024-12-13 13:41:53 -0800

    Getting vars

--- rules/implementation/NameLengthRule.ts
+++ rules/implementation/NameLengthRule.ts
@@ -5,12 +5,22 @@ export default class NameLengthRule extends ScanRule{
     inspect(whatToScan: Array<Node>,...args: any[]){
         // TODO: Varargs for inspect to support k/v pairs of configurable properties.
         // Approach will be as agnostic as possible to these
+        // const violatingNodes: Array<Node> = whatToScan
+        //     .filter(scannedNode => 
+        //         scannedNode.text != null && 
+        //         scannedNode.text.length <= 3 &&
+        //         this.RuleConfiguration.parentNodeTypeNames.includes(scannedNode.parent.type)
+        //     );
         const violatingNodes: Array<Node> = whatToScan
             .filter(scannedNode => 
                 scannedNode.text != null && 
-                scannedNode.text.length <= 3 &&
+                scannedNode.text.length > 0 &&
                 this.RuleConfiguration.parentNodeTypeNames.includes(scannedNode.parent.type)
             );
+
+        violatingNodes.forEach(node=>{
+            console.log(node.text);
+        })
             
         violatingNodes.forEach(node=>{
             this.addViolation(node);
