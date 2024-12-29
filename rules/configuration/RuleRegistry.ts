7d459b202eb82433692c5cf7757990ccc2c35c9a
Justin Stroud <157166970+justinstroudbah@users.noreply.github.com>
2024-12-29 12:07:29 -0800

    First pass at useful results, needs spot check

--- rules/configuration/RuleRegistry.ts
+++ rules/configuration/RuleRegistry.ts
@@ -21,3 +21,35 @@ export const RULE_REGISTRY = {
         "arguments":{"minimumLength":3}
     }]
 }
+
+export const MEASUREMENT_RULES = {
+    "rules":
+    [
+        {
+            "name":"variables",
+            "queries":[{
+                    "name":"Total",
+                    "query":'(variable_declarator (identifier) @constant)',
+                    "function":null
+                },{
+                    "name":"Length < 3",
+                    "query":'(variable_declarator (identifier) @exp (#match? @exp "^[a-zA-Z]{0,3}$"))',
+                    "function":null
+                }]
+        },
+        {
+            "name":"Class Declarations",
+            "queries":[{
+                    "name":"Total",
+                    "query":'(class_declaration) @constant',
+                    "function":null
+
+                },{
+                    "name":"No comment header",
+                    "query":'((block_comment) (class_declaration)) @exp',
+                    "function":function(node){return node.text.includes("@description")}
+                }]
+        },
+    ]
+}
+
