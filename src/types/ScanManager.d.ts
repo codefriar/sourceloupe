e825a464d834495143178d6fda660e9a97733841
Justin Stroud <157166970+justinstroudbah@users.noreply.github.com>
2025-01-25 17:08:33 -0800

    Again, raw push to master. Probably shouldn't do that.

--- src/types/ScanManager.d.ts
+++ src/types/ScanManager.d.ts
@@ -1,7 +1,9 @@
 import Parser from "tree-sitter";
 import Violation from "../core/Violation";
+import Rule from "./Rule";
 
 export default class ScanManager{
+    constructor(parser: Parser, language: any,sourcePath: string, sourceCode: string, rules: Array<Rule>);
     scan(): Map<string,Array<Violation>>;
     dump(queryString: string);
     measure(parser: Parser, language: any):  Map<string,Array<Violation>>;
