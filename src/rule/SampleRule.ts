1c39c8a6e462a905b5bdecc200dc8d9d6e17e897
Justin Stroud <157166970+justinstroudbah@users.noreply.github.com>
2025-01-30 22:04:27 -0800

    Prettier fix

--- src/rule/SampleRule.ts
+++ src/rule/SampleRule.ts
@@ -8,6 +8,6 @@ import { context, message, name, priority, query, regex, ScanRule, suggestion, c
 @priority(1)
 @query('((variable_declarator) identifier @expression)')
 @regex('foo')
-export default class SampleRule extends ScanRule {
+export class SampleRule extends ScanRule {
   // This is just a stub. The super returns true always
 }
