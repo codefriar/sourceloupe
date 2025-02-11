48540fe0c5d039446193bf5eab21926cc4a4aced
kpoorman <kjp@codefriar.com>
2025-02-11 12:10:03 -0800

    chore(exports): Exposing new classes and types
    Esposes:
    ScanResult, ResultType, Lanaguage
    
    Unexposes: ScanManager

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,10 @@
 // index.ts
 
-import ScanManager from './core/ScanManager';
 import Scanner from './core/Scanner';
 import { ScannerOptions } from './core/Scanner';
 import { ScanRule } from './rule/ScanRule';
+import ScanResult, { ResultType } from './results/ScanResult';
+import Langauge from './types/Language';
 
-export { ScanManager, ScanRule, Scanner };
-export type { ScannerOptions };
+export { ScanRule, Scanner, ScanResult };
+export type { ScannerOptions, ResultType, Langauge };
