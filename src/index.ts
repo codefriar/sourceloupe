66ee8af5bbf7f3435946b25843a0d50992a48ba4
Justin Stroud <157166970+justinstroudbah@users.noreply.github.com>
2025-02-11 15:54:46 -0800

    Disabled warnings related to annotations

--- src/index.ts
+++ src/index.ts
@@ -4,9 +4,9 @@ import Scanner from './core/Scanner';
 import { ScannerOptions } from './core/Scanner';
 import { ScanRule } from './rule/ScanRule';
 import ScanResult, { ResultType } from './results/ScanResult';
-import Langauge from './types/Language';
+import Language from './types/Language';
 import { context, message, name, priority, query, regex, suggestion, category } from './rule/ScanRule';
 
 export { ScanRule, Scanner, ScanResult };
 export { context, message, name, priority, query, regex, suggestion, category };
-export type { ScannerOptions, ResultType, Langauge };
+export type { ScannerOptions, ResultType, Language };
