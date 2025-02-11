25ceae3e5699492ac585e9718c80a20298075b4d
Justin Stroud <157166970+justinstroudbah@users.noreply.github.com>
2025-02-10 19:19:06 -0800

    0.0.32

--- src/__tests__/exampleRuleTest.ts
+++ src/__tests__/exampleRuleTest.ts
@@ -1,25 +1,22 @@
 /* eslint-disable prettier/prettier */
 import TsSfApex from 'tree-sitter-sfapex';
 import Scanner from '../core/Scanner';
-import {ExampleRule} from '../rule/ExampleRule';
+import { ExampleRule } from '../rule/ExampleRule';
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import ScanResult from '../results/ScanResult';
 
 test('Positive test for description', () => {
-
     //static async create(options: ScannerOptions): Promise<Scanner> {
-    
+
     const options = {
         rules: [new ExampleRule()],
         sourcePath: './src/__tests__/SampleApex.cls',
-        language: TsSfApex.apex
+        language: TsSfApex.apex,
     };
 
-    Scanner.create(options)
-        .then((scanner)=>{
-            scanner.run().then(results=>{
-                expect(results.size).toBeGreaterThan(0);
-            });
+    Scanner.create(options).then((scanner) => {
+        scanner.run().then((results) => {
+            expect(results.size).toBeGreaterThan(0);
         });
-
+    });
 });
